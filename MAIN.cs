6232ae4a2862e2d32217427aaf0c513b5c709955
opilane <opilane@HP-CZC2349HTC>
2025-09-22 08:37:55 +0300

    new

--- MAIN.cs
+++ MAIN.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Sõidukite;
+using System;
+using System.Collections.Generic;
+
 namespace Sõidukite
 {
     class Program
@@ -20,38 +24,72 @@ namespace Sõidukite
 
             if (tyyp == "auto")
             {
+                double kulu = 0;
+                double kaugus = 0;
+
                 Console.Write("Sisesta auto kütusekulu (liitrid/100km): ");
-                double kulu = double.Parse(Console.ReadLine());
+                while (!double.TryParse(Console.ReadLine(), out kulu) || kulu <= 0)
+                {
+                    Console.WriteLine("Palun sisesta kehtiv kütusekulu (positiivne arv).");
+                }
 
                 Console.Write("Sisesta auto läbitud kaugus (km): ");
-                double kaugus = double.Parse(Console.ReadLine());
+                while (!double.TryParse(Console.ReadLine(), out kaugus) || kaugus <= 0)
+                {
+                    Console.WriteLine("Palun sisesta kehtiv kaugus (positiivne arv).");
+                }
 
                 sõidukid.Add(new Auto(kulu, kaugus));
             }
             else if (tyyp == "jalgratas")
             {
+                double kaugus = 0;
+
                 Console.Write("Sisesta jalgratta läbitud kaugus (km): ");
-                double kaugus = double.Parse(Console.ReadLine());
+                while (!double.TryParse(Console.ReadLine(), out kaugus) || kaugus <= 0)
+                {
+                    Console.WriteLine("Palun sisesta kehtiv kaugus (positiivne arv).");
+                }
 
                 sõidukid.Add(new Jalgratas(kaugus));
             }
             else if (tyyp == "buss")
             {
+                double kulu = 0;
+                double kaugus = 0;
+                int reisijad = 0;
+
                 Console.Write("Sisesta bussi kütusekulu (liitrid/100km): ");
-                double kulu = double.Parse(Console.ReadLine());
+                while (!double.TryParse(Console.ReadLine(), out kulu) || kulu <= 0)
+                {
+                    Console.WriteLine("Palun sisesta kehtiv kütusekulu (positiivne arv).");
+                }
 
                 Console.Write("Sisesta bussi läbitud kaugus (km): ");
-                double kaugus = double.Parse(Console.ReadLine());
+                while (!double.TryParse(Console.ReadLine(), out kaugus) || kaugus <= 0)
+                {
+                    Console.WriteLine("Palun sisesta kehtiv kaugus (positiivne arv).");
+                }
+
 
                 Console.Write("Sisesta reisijate arv: ");
-                int reisijad = int.Parse(Console.ReadLine());
+                while (!int.TryParse(Console.ReadLine(), out reisijad) || reisijad < 0)
+                {
+                    Console.WriteLine("Palun sisesta kehtiv reisijate arv (positiivne arv).");
+                }
 
                 sõidukid.Add(new Buss(kulu, kaugus, reisijad));
             }
             else if (tyyp == "elektrilinetõukeratas")
             {
+                double kaugus = 0;
+
+ 
                 Console.Write("Sisesta elektrilise tõukeratta läbitud kaugus (km): ");
-                double kaugus = double.Parse(Console.ReadLine());
+                while (!double.TryParse(Console.ReadLine(), out kaugus) || kaugus <= 0)
+                {
+                    Console.WriteLine("Palun sisesta kehtiv kaugus (positiivne arv).");
+                }
 
                 sõidukid.Add(new ElektrilineTõukeratas(kaugus));
             }
@@ -71,12 +109,13 @@ namespace Sõidukite
             Console.WriteLine($"Kõigi sõidukite kütusekulu kokku: {kogukulu} liitrit");
         }
     }
+
+    // Interfaces and classes for each vehicle type would remain the same as in your original code
     public interface ISõiduk
     {
-
         double ArvutaKulu();
-
         double ArvutaVahemaa();
     }
 
+    // Add the rest of your class definitions for Auto, Jalgratas, Buss, ElektrilineTõukeratas
 }
